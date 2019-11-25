bb28ef77dc13bd212f91aa2877889f20ff32561d
TouhouBoi <48657244+TouhouBoi@users.noreply.github.com>
2019-11-25 15:44:59 -0500

    Create config.php

---
+++ grp_discovery-php/config.php
@@ -0,0 +1,15 @@
+<?php
+$config = [
+	'prod' => [
+		'api' => 'api-us.olv.nintendo.net',
+		'portal' => 'portal-us.olv.nintendo.net',
+		'n3ds' => '3ds-us.olv.nintendo.net'
+	],
+	'dev' => [
+		'api' => '',
+		'portal' => '',
+		'n3ds' => ''
+	],
+	'maintenanceMode' => false,
+	'serviceClosed' => false
+];
